586e2a5a35fc454c2afb5479d0e01d860b5e5bed
adityavarmachandika <adithyavarmachandika@gmail.com>
2025-05-20 22:45:27 +0530

    get latest mails

---
+++ config.ts
@@ -0,0 +1,63 @@
+import express from 'express';
+import { google } from 'googleapis';
+import open from 'open';
+import fs from 'fs'
+import dotenv from 'dotenv'
+import path from 'path'
+import { Request,Response } from 'express';
+import { OAuth2Client } from 'google-auth-library';
+dotenv.config({ path: path.resolve(__dirname, '../.env') })
+
+//required credentials for the oauth login
+const GOOGLE_CLIENT_ID=process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET=process.env.GOOGLE_CLIENT_SECRET;
+const GOOGLE_REDIRECT_URI=process.env.GOOGLE_REDIRECT_URI;
+const SCOPES=['https://www.googleapis.com/auth/gmail.readonly']
+
+
+//create client
+const client=new google.auth.OAuth2(GOOGLE_CLIENT_ID,GOOGLE_CLIENT_SECRET,GOOGLE_REDIRECT_URI)
+
+
+
+
+const authenticate=async ():Promise<OAuth2Client>=>{
+
+  //check if the user is already loged in recently
+  if(fs.existsSync('tokens.json')){
+    client.setCredentials(JSON.parse(fs.readFileSync('tokens.json').toString()))
+    return client;
+  }
+  else{
+    const authurl= client.generateAuthUrl({ access_type: 'offline', prompt: 'consent', scope: SCOPES })
+    console.log(authurl)
+    await open(authurl)
+    return new Promise((resolve)=>{
+      const app=express();
+
+      app.get('/callback',async (req:Request,res:Response)=>{
+        const code= req.query.code as string
+
+        try{
+            const {tokens}=await client.getToken(code);
+            fs.writeFileSync('tokens.json',JSON.stringify(tokens))
+            client.setCredentials(tokens)
+
+            console.log("all successfull")
+            res.json({"message":"this is successful"})
+            resolve(client);
+            server.close()
+        }
+        catch(error){
+          console.log(error)
+        }
+      })
+      const server = app.listen(3999, () => console.log('Listening on http://localhost:3999/'));
+    })
+
+    
+  }
+
+}
+
+export default authenticate
